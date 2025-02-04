99e9edc2b45859f1dbe1cc619721dce82f7b63ab
Dylan Monson <dylanmonson@Dylans-MacBook-Pro.local>
2025-02-04 17:42:37 -0500

    Initial Commit part 3

--- monsond/str-exercise.c
+++ monsond/str-exercise.c
@@ -3,14 +3,22 @@
 #include <string.h>
 
 int main(){
-    
 
+    char start = 'h';
+    char end = 'o';
     char s[] = "hello";
     int n = 5;
     int total = strlen(s);
     if (total == n){
-        printf("1");
+        printf("it has n characters\n\n");
         }
+    if (s[0] == start){
+        printf("the string starts with %c\n\n", start);
+    }
+    if (end == s[total-1]){
+        printf("the string ends with %c",end);
+    }
+    
     return 0;
         
     }
