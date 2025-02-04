803ccc845fb5860a0a7e6c789fc1be6ba2dc9253
Dylan Monson <dylanmonson@Dylans-MacBook-Pro.local>
2025-02-04 17:24:33 -0500

    Initial Commit part 2

--- monsond/str-exercise.c
+++ monsond/str-exercise.c
@@ -2,17 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 
-char s[] = "hello";
-int n = 5;
-
-
-
 int main(){
-(char *s, int n){
+    
 
+    char s[] = "hello";
+    int n = 5;
     int total = strlen(s);
     if (total == n){
-        return 1;
+        printf("1");
         }
     return 0;
         
