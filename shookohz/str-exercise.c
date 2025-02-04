e2ef7384a010d4487264cfa2419ba7f2d0876f41
shookohz <sshookohi.t@gmail.com>
2025-02-04 18:00:29 -0500

    Task1 code

--- shookohz/str-exercise.c
+++ shookohz/str-exercise.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(){
+int has_exact_n_chars(const char *str, int n) {
 
+    int i = 0;
+    while (str[i]!= '\0')
+    {
+        i++; 
+    }
+    
+    return i == n;
 
-    printf("hi\n");
 }
