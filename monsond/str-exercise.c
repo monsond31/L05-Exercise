13820dd852acc73a5eaffe188d265e9f266e3459
Dylan Monson <dylanmonson@Dylans-MacBook-Pro.local>
2025-02-04 17:10:48 -0500

    Initial commit

---
+++ monsond/str-exercise.c
@@ -0,0 +1,20 @@
+//this function will return one if the string has exactly n characters
+#include <stdio.h>
+#include <string.h>
+
+char s[] = "hello";
+int n = 5;
+
+
+
+int main(){
+(char *s, int n){
+
+    int total = strlen(s);
+    if (total == n){
+        return 1;
+        }
+    return 0;
+        
+    }
+
